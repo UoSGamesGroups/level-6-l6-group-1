d159bf9baa42442e146e9a0db32025a3e357e902
matt allum <matt allum>
2016-10-13 17:32:22 +0100

    Next stage in furniture moving code: Sends move command to each localcontroller, and thus they're transform locations

---
+++ Unity Files/Of Whom We Hold Dear/Assets/Code/GameController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class GameController : MonoBehaviour {
+
+	public float timer;
+	public int reset;
+	public TextMesh CountTimer;
+	public bool resetComplete;
+    private int replyCount;
+
+    public List<LocalController> LocalControllers;
+
+    public int ReplyCount {
+        get {
+            return replyCount;
+        }
+    }
+
+    public void RegisterLocalController(LocalController controller) 
+    {
+        LocalControllers.Add(controller);
+    }
+
+    // Use this for initialization
+    void Start () {
+		resetComplete = true;
+	}
+	
+    public void LocalComplete() {
+        replyCount++;
+    }
+
+    private void TriggerGeneration() 
+    {
+        foreach (LocalController local in LocalControllers) {
+            local.GenerateLocations();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+		CountTimer.text = timer.ToString("F1");
+
+		if (timer >= 0 ) {
+			timer -= Time.deltaTime;
+		} 
+		if (timer < 0 ){
+			timer = reset;
+            TriggerGeneration();
+		}
+    }
+}
--- Unity Files/Of Whom We Hold Dear/Assets/Code/LocalController.cs
+++ Unity Files/Of Whom We Hold Dear/Assets/Code/LocalController.cs
@@ -15,96 +15,89 @@ public class LocalController : MonoBehaviour {
 
 	public float Timer;
 	public bool ChangeRoom;
+    public int replyCount;
 
 	public itemLocation ItemLocation;
 
 	public GameController gamecontroller;
 
-
-
 	void Awake()
 	{
 		gamecontroller = GameObject.FindGameObjectWithTag("NoticeBoard").GetComponent<GameController>();
 		ChangeRoom = true;
+        gamecontroller.RegisterLocalController(this);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-		Timer = gamecontroller.timer;
-
-
-		//If timer reaches zero run the checks
-
-		if (!gamecontroller.resetComplete && ChangeRoom) {
-
-			for (int i = 0; i <itemTransformLocation.Length; i++){
-
-
-				int caseRandom = Random.Range (0,5);
-				print(caseRandom);
-
-				switch(caseRandom){
-				
+    public void GenerateLocations() 
+    { 
+        for (int i = 0; i<itemTransformLocation.Length; i++)
+        {
+			int caseRandom = Random.Range(0, 5);
+            print(caseRandom);
+			switch(caseRandom)
+            {
 ///////////////////////////////////////////////////////////////////////////////////////
-				case 0:
-					foreach(GameObject item in Chairs){
+			case 0:
+				foreach(GameObject item in Chairs){
 
 
-						break;
-					}	
-				break;
-////////////////////////////////////////////////////////////////////////////////////////
-
-				case 1:
-					foreach(GameObject item in Bed){
-						
-						
-						break;
-					}
-				break;
-////////////////////////////////////////////////////////////////////////////////////////
-
-				case 2:
-					foreach(GameObject item in ChestDrawers){
-						
-						
-						break;
-					}
-				break;
-////////////////////////////////////////////////////////////////////////////////////////
-
-				case 3:
-					foreach(GameObject item in KitchenCounter){
-						
-						
-						break;
-					}
-				break;
-/////////////////////////////////////////////////////////////////////////////////////////
-
-				case 4:
-					foreach(GameObject item in Rug){
-						
-						
-						break;
-					}
 					break;
+				}	
+			break;
+////////////////////////////////////////////////////////////////////////////////////////
+
+			case 1:
+				foreach(GameObject item in Bed){
+						
+						
+					break;
+				}
+			break;
+////////////////////////////////////////////////////////////////////////////////////////
+
+			case 2:
+				foreach(GameObject item in ChestDrawers){
+						
+						
+					break;
+				}
+			break;
+////////////////////////////////////////////////////////////////////////////////////////
+
+			case 3:
+				foreach(GameObject item in KitchenCounter){
+						
+						
+					break;
+				}
+			break;
 /////////////////////////////////////////////////////////////////////////////////////////
-				}
 
+			case 4:
+				foreach(GameObject item in Rug){
+						
+						
+					break;
 				}
-			ChangeRoom = false;
-			StartCoroutine(WaitSeconds(5f));
-			gamecontroller.resetComplete = true;
-			ChangeRoom = true;
-
+				break;
+/////////////////////////////////////////////////////////////////////////////////////////
 			}
+		ChangeRoom = false;
 		}
+    }
 
-		IEnumerator WaitSeconds(float waitsec) {
-		yield return new WaitForSeconds(waitsec);
-	}
+    // Update is called once per frame
+    void Update() {
 
 
+        Timer = gamecontroller.timer;
+        replyCount = gamecontroller.ReplyCount;
+
+
+        if (replyCount >= 7) {
+            ChangeRoom = true;
+        }
+
+        //If timer reaches zero run the checks
+    }
 }
