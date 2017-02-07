b04d836cc127e8753882fa87ecbac9cc94efdcd6
Aaron Mulligan <aaron95mulligan@gmail.com>
2017-02-07 20:46:44 +0000

    - Added to meeting minutes - Added to sprint hours - Fixed code to allow players to look at letters and object to identify what can be used - removed old version of look at script

---
+++ Unity Files/Of Whom We Hold Dear/Assets/Code/HighlightLetters.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightLetters : MonoBehaviour
+{
+
+    private Color StartColor;
+
+    void OnMouseEnter()
+    {
+        StartColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+    
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = StartColor;
+    } 
+ }
